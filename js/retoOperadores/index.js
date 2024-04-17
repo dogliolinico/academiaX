7d6777d41da006895f88d37c3c2070615382fea9
NicoDev <dogliolinico@gmail.com>
2024-04-17 23:39:33 +0100

    retoOperadores completo

---
+++ js/retoOperadores/index.js
@@ -0,0 +1,12 @@
+
+console.log(`El numero 1 es mayor que el numero 2 : ` + (1 > 2));
+console.log(`El numero 1 es menor que el numero 2 : ` + (1 < 2));
+console.log(`El numero 1 es igual que el numero 2 : ` + (1 == 2));
+console.log(`Los objetos son iguales ? : ` + (1 === 2));
+console.log(`AND logico : ` + (1 < 2 && 1 < 3));
+console.log(`OR logico : ` + (1 < 2 || 1 < 3));
+console.log(`la suma 10 + 3  : ` + (10 + 3));
+console.log(`la resta 10 - 3  : ` + (10 - 3));
+console.log(`la multiplicacion 10 * 3  : ` + (10 * 3));
+console.log(`la division 10 / 3  : ` + (10 / 3));
+console.log(`10 modulo 3   : ` + (10 % 3));
